36a075265efcc5f7181871c3abf903e647c3e651
Abhyuday Pandey <34156497+abhyuday07@users.noreply.github.com>
2018-11-02 01:46:26 +0530

    Update satdplla.cpp

--- satdplla.cpp
+++ satdplla.cpp
@@ -15,7 +15,18 @@
 using namespace std;
 int cnt=0;
 int clauses=0;
+int var=0;
 set<int> currassgn;
+int max_occ(vector<set<int>> query){
+	int pos[var+1],occur[var+1];
+	for(int i=0;i<var+1;i++){ occur[i]=0; pos[i]=0; }
+	for(std::vector<set<int>>::iterator it=query.begin();it!=query.end();it++){
+		for(auto j=it->begin();j!=it->end();j++){  occur[abs(*j)]++; if(*j>0) pos[*j]++; }
+	}
+	int idx=distance(occur,max_element(occur+1,occur+var+1));
+	if(2*pos[idx]>=occur[idx]) return idx;
+	else return -idx;
+}
 pair<int,int> isempty(vector<set<int>> query){
 	pair<int,int> check;
 	check.first=0;
@@ -42,7 +53,6 @@ vector<set<int>> eliminateall(vector<set<int>> query, int rem){
 	return query1;
 }
 int solve(vector<set<int>> query){
-	cnt++;
 	pair<int,int> check=isempty(query);
 
 	if(check.first==1){
@@ -52,14 +62,14 @@ int solve(vector<set<int>> query){
 	if(check.first==-1){
 		return 1;
 	}
-
+	cnt++;
 	if(check.second!=0){
 		currassgn.insert(check.second);
 		vector<set<int>> query1=eliminateall(query,check.second);
 		return solve(query1);
 	}
 
-	int l=*((query.begin())->begin());
+	int l=max_occ(query);
 	set<int> base1,base2;
 	base1.insert(l);
 	base2.insert(-l);
@@ -75,12 +85,11 @@ int solve(vector<set<int>> query){
 }
 int main(){
 	clock_t start=clock();
-	FILE* fp=fopen("sat1.txt","r");
-	int var=0;
+	FILE* fp=fopen("sat5.txt","r");
 	fscanf(fp,"p cnf %d %d",&var,&clauses);
 	vector<set<int>> query;
 	int temp=0;
-	int arr[2*var+1];
+	bool arr[2*var+1];
 	for(int i=0;i<2*var+1;i++) arr[i]=0;
 	set<int> empty;
 	for(int i=0; i<clauses;i++) query.push_back(empty);
@@ -88,23 +97,25 @@ int main(){
 		while(1){
 			fscanf(fp,"%d",&temp);
 			if(temp==0) break;
-			else if(temp>0) arr[temp]++;
-			else arr[var+abs(temp)]++; 
+			else if(temp>0) arr[temp]=1;
+			else arr[var+abs(temp)]=1; 
 			query[i].insert(temp);
 		}
 	}
-	//Remove Pure literals
+	//pure literals
 	for(int i=1;i<=var;i++){
 		if(arr[i]&&!arr[var+i]){ currassgn.insert(i); query.erase(remove_if(query.begin(),query.end(),[&i](set<int> j){return j.find(i)!=j.end();}),query.end());}
 		else if(!arr[i]&&arr[var+i]){ currassgn.insert(-i); query.erase(remove_if(query.begin(),query.end(),[&i](set<int> j){return j.find(-i)!=j.end();}),query.end());}
 	}
-	//Remove tautology
-	// for(int i=1;i<=var;i++){
-	// 	for(std::vector<set<int>>::iterator it=query.begin();it!=query.end();it++){
-	// 		if(it->find(var)!=it->end()&&it)
-	// 	}
-	// }
+	//remove tautology
+	for(int i=1;i<=var;i++){
+		for(std::vector<set<int>>::iterator it=query.begin();it!=query.end();){
+			if(it->find(i)!=it->end()&&it->find(-i)!=it->end()) it=query.erase(it);
+			else it++;
+		}
+	}
 	if(solve(query)) cout<<"SAT"<<endl;
+	// cout<<max_occ(query,var);
 	else cout<<"UNSAT"<<endl;
 	cout<<"----------------"<<endl;
 	for(auto it1=currassgn.begin();it1!=currassgn.end();it1++){

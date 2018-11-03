16513207df3aebdffed5f94c98459c5063cdfa05
srinjayk <srinjayiitkanpur@gmail.com>
2018-11-03 13:20:15 -0400

    Added readme and commented the code

--- README.md
+++ README.md
@@ -17,10 +17,19 @@ The output will consist of four parts:
 3. Number of calls to DP.
 4. Execution time in seconds
 
-Heuristics 
+Heuristics
 -------
 We have applied the following heuristics:
-1. Pure Literals: They are the literals that for which their negation is not present in any of the clauses.
-2. Removing Tautologies: They are the clauses in which 'a' and '-a' both are present.
-3. Maximum Occurence: We proceed in solving by removing a literal with maximum occurence. It will be useful since will reduce the size of maximum number of clauses.
-4.
+1. Pure Literals: They are the literals that for which their negation is not present in any of the clauses. 
+2. Maximum Occurrence: We proceed in solving by removing a literal with maximum occurence. It will be useful since will reduce the size of maximum number of clauses.
+
+Some Other Steps
+----------------
+1. We remove the singular clauses(the clauses with only one literal).
+2. The literals present in the singular clauses are assigned true and they are then removed.
+3. If the literal is 'a' the all the clauses containing 'a' are removed and '-a' is removed from each clause.
+
+Compile and Run
+---------------
+1. g++ -std=c++17 satdpll.cpp
+2. ./a.out
--- satdpll.cpp
+++ satdpll.cpp
@@ -13,10 +13,13 @@
 #include<thread>
 #include<chrono>
 using namespace std;
+//this variable will count the no of calls made
 int cnt=0;
 int clauses=0;
 int var=0;
+//set currassgn will comtain already satisfied literals
 set<int> currassgn;
+//this fuction will return the return the literal which will have maximum occurence
 int max_occ(vector<set<int>> query){
 	int pos[var+1],occur[var+1];
 	for(int i=0;i<var+1;i++){ occur[i]=0; pos[i]=0; }
@@ -27,13 +30,15 @@ int max_occ(vector<set<int>> query){
 	if(2*pos[idx]>=occur[idx]) return idx;
 	else return -idx;
 }
+
+//this will check for pure literals
 int is_pure(vector<set<int>> query){
 	bool arr[2*var+1];
 	for(int i=0;i<2*var+1;i++) arr[i]=0;
 	for(std::vector<set<int>>::iterator it=query.begin();it!=query.end();it++){
 		for(set<int>::iterator j=it->begin();j!=it->end();j++){
 			if(*j>0) arr[*j]=1;
-			else arr[var+abs(*j)]=1; 
+			else arr[var+abs(*j)]=1;
 		}
 	}
 	for(int i=1;i<=var;i++){
@@ -41,9 +46,13 @@ int is_pure(vector<set<int>> query){
 	}
 	return 0;
 }
+
+//this function will eliminate all the pure literals
 pair<vector<set<int>>,set<int>> purify(vector<set<int>> query){
 	bool arr[2*var+1];
+	//query1 will be a copy for query
 	vector<set<int>> query1;
+	//this set will contain all the pure literals
 	set<int> pure;
 	for(std::vector<set<int>>::iterator it=query.begin();it!=query.end();it++){
 		query1.push_back(*it);
@@ -52,13 +61,15 @@ pair<vector<set<int>>,set<int>> purify(vector<set<int>> query){
 	for(std::vector<set<int>>::iterator it=query.begin();it!=query.end();it++){
 		for(set<int>::iterator j=it->begin();j!=it->end();j++){
 			if(*j>0) arr[*j]=1;
-			else arr[var+abs(*j)]=1; 
+			else arr[var+abs(*j)]=1;
 		}
 	}
+	//entering values in the set named pure
 	for(int i=1;i<=var;i++){
 		if(arr[i]&&!arr[var+i]) pure.insert(i);
 		else if(!arr[i]&&arr[var+i]) pure.insert(-i);
 	}
+	//erasing pure literals from query1
 	for(std::set<int>::iterator it=pure.begin(); it!=pure.end();it++){
 		int rem=*it;
 		query1.erase(remove_if(query1.begin(),query1.end(),[&rem](set<int> i){return i.find(rem)!=i.end();}),query1.end());
@@ -68,6 +79,8 @@ pair<vector<set<int>>,set<int>> purify(vector<set<int>> query){
 	q.second=pure;
 	return q;
 }
+
+//this fucnction will check for empty clauses
 pair<int,int> isempty(vector<set<int>> query){
 	pair<int,int> check;
 	check.first=0;
@@ -81,6 +94,8 @@ pair<int,int> isempty(vector<set<int>> query){
 	if(all==1) check.first=-1;
 	return check;
 }
+
+//this function eliminates all the singular clauses i.e. the clauses with only one literal
 pair<vector<set<int>>,set<int>> eliminateall(vector<set<int>> query){
 	vector<set<int>> query1;
 	set<int> singular;
@@ -102,6 +117,8 @@ pair<vector<set<int>>,set<int>> eliminateall(vector<set<int>> query){
 	val.first=query1;
 	val.second=singular;	return val;
 }
+
+//this function will solve the clauses and return either SAT(1) or UNSAT(0)
 int solve(vector<set<int>> query){
 	pair<int,int> check=isempty(query);
 
@@ -112,7 +129,7 @@ int solve(vector<set<int>> query){
 		pair<vector<set<int>>,set<int>> q=eliminateall(query);
 		if(solve(q.first)==1){
 			for(std::set<int>::iterator iter=(q.second).begin();iter!=(q.second).end();iter++)
-			currassgn.insert(*iter); 
+			currassgn.insert(*iter);
 			return 1;
 		}
 		else return 0;
@@ -121,14 +138,17 @@ int solve(vector<set<int>> query){
 		pair<vector<set<int>>,set<int>> q=purify(query);
 		if(solve(q.first)==1){
 			for(std::set<int>::iterator iter=(q.second).begin();iter!=(q.second).end();iter++)
-			currassgn.insert(*iter); 
+			currassgn.insert(*iter);
 			return 1;
 		}
 		else return 0;
 	}
+
 	//int l=max_occ(query);
 	// for(auto it=(query.begin())->begin();it!=(query.begin())->end();it++)
 	// 	cout<<"---"<<*it<<"---";
+
+	// Here we knock out the literal which has maximum occurence  and the recursively solve the problem
 	int l=max_occ(query);
 	set<int> base1,base2;
 	base1.insert(l);
@@ -143,22 +163,28 @@ int solve(vector<set<int>> query){
 	}
 
 }
+
+
 int main(){
 	clock_t start=clock();
 	FILE* fp=fopen("sat.txt","r");
 	fscanf(fp,"p cnf %d %d\n",&var,&clauses);
+	//query is a vector of sets
 	vector<set<int>> query;
 	int temp=0;
+	//this boolean array will store the occurence of both positive and negated literals
 	bool arr[2*var+1];
 	for(int i=0;i<2*var+1;i++) arr[i]=0;
 	set<int> empty;
 	for(int i=0; i<clauses;i++) query.push_back(empty);
+
+	//here, we take input to 'query' and also assign the values for boolean array
 	for(int i=0; i<clauses;i++){
 		while(1){
 			fscanf(fp,"%d",&temp);
 			if(temp==0) break;
 			else if(temp>0) arr[temp]=1;
-			else arr[var+abs(temp)]=1; 
+			else arr[var+abs(temp)]=1;
 			query[i].insert(temp);
 		}
 	}
@@ -168,12 +194,15 @@ int main(){
 		else if(!arr[i]&&arr[var+i]){ currassgn.insert(-i); query.erase(remove_if(query.begin(),query.end(),[&i](set<int> j){return j.find(-i)!=j.end();}),query.end());}
 	}
 	//remove tautology
+	//here we find if any clause has both 'g' and '-g'
 	for(int i=1;i<=var;i++){
 		for(std::vector<set<int>>::iterator it=query.begin();it!=query.end();){
 			if(it->find(i)!=it->end()&&it->find(-i)!=it->end()) it=query.erase(it);
 			else it++;
 		}
 	}
+
+	//this part prints the output of the code
 	if(solve(query)){
 		cout<<"SATISFIABLE"<<endl;
 		cout<<"-----------------"<<endl;
